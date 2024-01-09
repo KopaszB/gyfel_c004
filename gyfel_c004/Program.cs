76704a9a4f373ffef96196a368352a6d7a3a9378
user <user@LENOVOL15G1-15>
2024-01-09 20:46:01 +0100

    Add project files.

---
+++ gyfel_c004/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//C.4.feladat: Nem negatív egész számról határozza meg a program, hogy hány jegyű!
+
+namespace gyfel_c004
+{
+    class Program
+    {
+        static void Main()
+        {
+            int szam = int.Parse(Console.ReadLine());
+            Console.WriteLine($"A {szam} számjegyeinek a száma - {SzamjegyekSzama(szam)}"); 
+            Console.ReadLine();
+
+        }
+        static int SzamjegyekSzama(int szam)
+        {
+            int szamlalo = 1;
+            int i = 0;
+            while (szam >= 10)
+            {
+                szam = szam / 10;
+                szamlalo++;
+                i++;
+            }
+            return szamlalo;
+        }
+    }
+}
